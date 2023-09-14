3df011e923b5df6b47673ba8a61cd1bc75a97869
KTS <gamets99@gmail.com>
2023-09-14 21:35:03 +0900

    right

--- frontend/src/Components/app/Navi.js
+++ frontend/src/Components/app/Navi.js
@@ -9,23 +9,61 @@ function Navi() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">홈</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav justify-content-between">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
+                        <NavDropdown title="구독채널" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">
+                                1
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                2
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">
+                                3
+                            </NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">
-                                Separated link
+                                4
                             </NavDropdown.Item>
                         </NavDropdown>
+                        <NavDropdown title="공식채널" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">
+                                1
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                2
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">
+                                3
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                4
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#link">채널검색</Nav.Link>
+                        
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <NavDropdown title="로그인" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">
+                                1
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                2
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">
+                                3
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                4
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        
+                        <Nav.Link href="#link">채널검색</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- frontend/src/Components/router/Router.js
+++ frontend/src/Components/router/Router.js
@@ -3,6 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../app/Home"
 import BoardList from "../board/BoardList"
 import BoardDetail from "../board/BoardDetail"
+import Join from "../member/Join"
+import Login from "../member/Login"
+import Logout from "../member/Logout"
+
 
 function Router() {
 	return (
@@ -10,6 +14,11 @@ function Router() {
 			<Route path="/" element={<Home />}></Route>
 			<Route path="/board" element={<BoardList />}></Route>
 			<Route path="/board/:boardId/" element={<BoardDetail />}></Route>
+			<Route path="/" element={<Home />}></Route>
+
+			<Route path="/login" element={<Login />}></Route>
+			<Route path="/join" element={<Join />}></Route>
+			<Route path="/logout" element={<Logout />}></Route>
 		</Routes>
 	);
 }
